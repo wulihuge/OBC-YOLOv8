6b1356bd09af4d2aed5b829906fc8b1c8ba72369
wulihuge <1062698149@qq.com>
2024-10-09 07:41:42 +0800

    Update draw.py

--- ultralytics10.24/draw.py
+++ ultralytics10.24/draw.py
@@ -6,9 +6,9 @@ def clean_column_names(df):
     df.columns = df.columns.str.strip()
     df.columns = df.columns.str.replace('\s+', '_', regex=True)
  
-#nonoresult.csv表示原始的结果图,csv文件在runs/train/exp中
+
 original_results = pd.read_csv("/home/lzh/ultralytics10.24/runs/detect/train52/results.csv")
-#yesyesresult.csv表示提高后的结果图，csv文件在runs/train/exp中
+
 improved_results = pd.read_csv("/home/lzh/ultralytics10.24/runs/detect/train64/results.csv")
  
 # Clean column names
@@ -17,7 +17,7 @@ clean_column_names(improved_results)
  
 # Plot mAP@0.5 curves
 plt.figure()
-#lable属性为曲线名称，自己可以定义
+
 plt.plot(original_results['metrics/mAP50(B)'], label="YOLOv8n")
 plt.plot(improved_results['metrics/mAP50(B)'], '-.', label="OBC-YOLOv8")
 plt.xlabel("Epoch")
@@ -33,8 +33,8 @@ plt.plot(improved_results['metrics/mAP50-95(B)'], '-.', label="OBC-YOLOv8")
 plt.xlabel("Epoch")
 plt.ylabel("mAP@0.5:0.95")
 plt.legend()
-#图的标题
+
 plt.title("mAP@0.5:0.95 Comparison")
-#图片名称
+
 plt.savefig("mAP_0.5_0.95_comparison_02.png")
  
